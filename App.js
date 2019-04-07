d307db1ee7445a081f3d996c7c2160501581298a
CAWRESTLER95 <jreyes@cpp.edu>
2019-04-07 01:19:28 -0700

    Update App.js

--- App.js
+++ App.js
@@ -46,7 +46,7 @@ class ProfileScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>This is the Profile Screen Were finally working on it you slut</Text>
+        <Text>This is the Profile Screen Were  working on it you slut</Text>
       </View>
     );
   }
